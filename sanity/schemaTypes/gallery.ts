95ffaed18a2d689ac909bd2ef115687d9d0fd0e6
Marek Gacek <marekgacek45@gmail.com>
2024-12-14 07:38:48 +0100

    set up sanity and create singleton gallery document

---
+++ sanity/schemaTypes/gallery.ts
@@ -0,0 +1,27 @@
+
+import { Image } from 'lucide-react'
+import { defineField, defineType } from 'sanity'
+
+
+export const gallery = defineType({
+    name: 'gallery',
+    title: 'Galeria',
+    type: 'document',
+
+    fields: [
+        defineField({
+            name: 'title',
+            title: 'Tytuł',
+            type: 'string',
+            validation: (Rule) => Rule.required(),
+        }),
+        defineField({
+            name: 'images',
+            title: 'Zdjęcia',
+            type: 'array',
+            of: [{ type: 'image' }],
+            validation: (Rule) => Rule.required(),
+        }),
+    ],
+    
+})
